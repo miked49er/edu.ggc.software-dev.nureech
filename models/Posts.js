02afe33214fa4d1bc81004d17bbcad0ff0c5cdf8
Mike Deiters <miked49er@gmail.com>
2016-01-30 16:46:07 -0500

    Had changed the name of Posts.js to Reeches.js, but did not commit the removal of Posts.js

--- models/Posts.js
+++
@@ -1,8 +0,0 @@
-var mongoose = require('mongoose');
-
-var PostSchema = new mongoose.Schema({
-  body: String,
-  comments: [{type: mongoose.Schema.Types.ObjectId, ref: 'Comment'}]
-});
-
-mongoose.model('Post', PostSchema);
