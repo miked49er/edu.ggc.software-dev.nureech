0e3177a3a625ba23b028284b5cd7d13294ee6906
Mike Deiters <miked49er@gmail.com>
2016-01-30 21:25:08 -0500

    Added author and timestamp fields to the Schema

--- models/Reeches.js
+++ models/Reeches.js
@@ -2,6 +2,8 @@ var mongoose = require('mongoose');
 
 var ReechSchema = new mongoose.Schema({
   body: String,
+  author: String,
+  timestamp: {type: Date, default: Date.now},
   comments: [{type: mongoose.Schema.Types.ObjectId, ref: 'Comment'}]
 });
 
