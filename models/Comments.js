41abcaee8f84c6964e1eb4f10846841fe63752e4
Mike Deiters <miked49er@gmail.com>
2016-01-30 21:27:00 -0500

    Added timestamp the the CommentSchema

--- models/Comments.js
+++ models/Comments.js
@@ -3,6 +3,7 @@ var mongoose = require('mongoose');
 var CommentSchema = new mongoose.Schema({
   body: String,
   author: String,
+  timestamp: {type: Date, default: Date.now},
   reeches: {type: mongoose.Schema.Types.ObjectId, ref: 'Reech'}
 });
 
